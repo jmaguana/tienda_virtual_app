350659143782cb86ce33db987ffbcbac21571700
jmaguana <53019106+jmaguana@users.noreply.github.com>
2020-02-04 10:33:45 -0500

    implemntado funcion like

--- lib/servicios.dart
+++ lib/servicios.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:tienda_virtual/modelo.dart';
 
-final servicioUrl = 'http://172.16.216.248:8080/tienda-virtual/srv/';
+final servicioUrl = 'http://192.168.0.106:8080/tienda-virtual/srv/';
 
 Future<Cliente> login(String correo, String contrasenia) async {
   String aux = servicioUrl+'clientes/login/'+correo.replaceAll(" ", "")+"/"+contrasenia;
@@ -87,6 +87,48 @@ Future<String> generarCompra(int id) async{
   }
 }
 
+Future<Producto> buscarProducto(int id) async{
+  final response = await http.get(servicioUrl+'productos/buscar/'+id.toString());
+  if(response.statusCode == 200){
+    return Producto.fromJson(json.decode(response.body));
+  }else{
+    throw Exception('Fallo agregando a carrito');
+  }
+}
+
+Future<bool> isLiked(int idClinete, int idProducto) async{
+  final response = await http.get(servicioUrl+'productos/islike/'+idProducto.toString()+"/"+idClinete.toString());
+  if(response.statusCode == 200){
+    print(response.body);
+    return response.body.toLowerCase() == 'true';
+  }else{
+    throw Exception('Fallo agregando a carrito');
+  }
+}
+
+Future<void> darLike(int idClinete, int idProducto, bool isLiked) async{
+
+  if(isLiked){
+    final response = await http.get(servicioUrl+'productos/like/'+idProducto.toString()+"/"+idClinete.toString()+"/0");
+
+    if(response.statusCode == 200){
+      print(response.body);
+
+    }else{
+      throw Exception('Fallo agregando a carrito');
+    }
+  }else{
+    final response = await http.get(servicioUrl+'productos/like/'+idProducto.toString()+"/"+idClinete.toString()+"/1");
+    if(response.statusCode == 200){
+      print(response.body);
+
+    }else{
+      throw Exception('Fallo dar like');
+    }
+  }
+
+}
+
 Future<void> agregarCarrito(int cantidad, int codigoProducto, int codigoCliente) async{
   final response = await http.get(servicioUrl+'carrito/insertar/'+codigoCliente.toString()+'/'+codigoProducto.toString()+'/'+cantidad.toString());
   if(response.statusCode == 200){
@@ -94,5 +136,4 @@ Future<void> agregarCarrito(int cantidad, int codigoProducto, int codigoCliente)
   }else{
     throw Exception('Fallo agregando a carrito');
   }
-  ///srv/carrito/insertar/{codigoCliente:int}/{codigoProducto:int}/{cantidad:int}
 }
